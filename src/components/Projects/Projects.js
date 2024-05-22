30ee54a3e9f17ced8d3a48a87f378ca16361840e
robertoclunarc <robertoclunarg@gmail.com>
2024-05-22 18:20:17 -0400

    add projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
-import unichat from "../../Assets/Projects/Unichat.jpg";
+//import unichat from "../../Assets/Projects/Unichat.jpg";
 import desbloqueamicell from "../../Assets/Projects/desbloqueamicell.png";
 import mcoc from "../../Assets/Projects/marvel.png";
-import youtube from "../../Assets/Projects/youtube.jpg";
-import mysuru from "../../Assets/Projects/mysuru.JPG";
-import pollution from "../../Assets/Projects/pollution.png";
+import sisca from "../../Assets/Projects/sisca.png";
+import sisSalud from "../../Assets/Projects/sissalud";
+//import pollution from "../../Assets/Projects/pollution.png";
 
 function Projects() {
   return (
@@ -42,39 +42,21 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={youtube}
-              title="Youtube Clone"
-              description="I have created this project for my Open Source submission. This webste is basically the frontend clone of Youtube which uses Youtube API to fetch videos from Youtube. I have created this project using React, Redux and Firebase."
-              link="https://github.com/sagarmude7/Youtube-Clone"
+              imgPath={sisca}
+              title="Sistema Control de Asistencia"
+              description="Attendance control system to report the time worked of each worker and their incidents"
+              link="https://github.com/robertoclunarc/sist-briqven/tree/main/control_asistencia"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={unichat}
-              title="UniChat"
-              description="This is a real-time chat application build using React.js, Firebase and React chat engine. Here you can do one to one and one to many chats by creating groups and adding the members to it."
-              link="https://github.com/sagarmude7/UniChat"
+              imgPath={sisSalud}
+              title="Sistema de Gestion de Salud Laboral"
+              description="Occupational health management system to record all the medical history of each worker"
+              link="https://github.com/robertoclunarc/coreui-admin"
             />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={mysuru}
-              title="Mysuru Tourism Website"
-              description="I have created this website for DSC-WOW Hackathon. This website is created with basic HTML, CSS and JavaScript. This website helps to increase tourism in Mysore by featuring all the toursits spots."
-              link="https://github.com/sagarmude7/DSCWOW_Mysuru-website"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={pollution}
-              title="Pollution Tracker"
-              description="It is a python program that tells the API (Air Pollution Index) and further pollutants level in air of any city in the world. It uses Matplotlib to plot pie chart of pollutants in air."
-              link="https://github.com/sagarmude7/Pollution-Tracker"
-            />
-          </Col>
+          </Col>          
         </Row>
       </Container>
     </Container>
